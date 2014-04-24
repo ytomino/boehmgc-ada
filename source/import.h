5ba4cc44631678526ce0c957f9154eb4c7370439
yt <demoonlit@panathenaia.halfmoon.jp>
2014-04-25 04:01:39 +0900

    Fix for FreeBSD9

--- source/import.h
+++ source/import.h
@@ -5,3 +5,10 @@
 #if defined(__linux__)
 #pragma for Ada "sys/mman.h" include "bits/mman.h"
 #endif
+
+#if defined(GC_RETURN_ADDR)
+#undef GC_RETURN_ADDR
+#endif
+#if defined(GC_RETURN_ADDR_PARENT)
+#undef GC_RETURN_ADDR_PARENT
+#endif
