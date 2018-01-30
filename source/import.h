19a72fef36a9ed23e455bc87b0094f683a509f1c
ytomino <demoonlit@panathenaia.halfmoon.jp>
2018-01-30 17:22:05 +0900

    Fix import.h for glibc >= 2.18

--- source/import.h
+++ source/import.h
@@ -3,7 +3,11 @@
 #include <sys/mman.h> /* mprotect */
 
 #if defined(__linux__)
+#if !__GLIBC_PREREQ(2, 18)
 #pragma for Ada "sys/mman.h" include "bits/mman.h"
+#else
+#pragma for Ada "sys/mman.h" include "bits/mman-linux.h"
+#endif
 #endif
 
 #if defined(GC_RETURN_ADDR)
