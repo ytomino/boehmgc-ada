628c798f662ec0a7a173700368948ff1cf747017
ytomino <demoonlit@panathenaia.halfmoon.jp>
2025-03-25 15:51:58 +0900

    Use GC_get_version
    GC_version could not be linked with the recent bdw-gc.

--- source/gc.adb
+++ source/gc.adb
@@ -2,34 +2,24 @@ with C.gc.gc;
 package body GC is
 	use type C.unsigned_int;
 	
-	-- the version variable is not declared in header files
-	GC_version : C.unsigned_int
-		with Import, Convention => C, External_Name => "GC_version";
-	
 	-- implementation
 	
 	function Version return String is
+		GC_version : constant C.unsigned_int := C.gc.gc.GC_get_version;
 		Major : constant C.unsigned_int := C.Shift_Right (GC_version, 16);
 		Minor : constant C.unsigned_int :=
 			C.Shift_Right (GC_version, 8) and (2 ** 8 - 1);
-		Alpha : constant C.unsigned_int := GC_version and (2 ** 8 - 1);
+		Micro : constant C.unsigned_int := GC_version and (2 ** 8 - 1);
 		Major_Image : constant String := C.unsigned_int'Image (Major);
 		Minor_Image : constant String := C.unsigned_int'Image (Minor);
+		Micro_Image : constant String := C.unsigned_int'Image (Micro);
 	begin
 		pragma Assert (Major_Image (Major_Image'First) = ' ');
 		pragma Assert (Minor_Image (Minor_Image'First) = ' ');
-		if Alpha /= 255 then
-			declare
-				Alpha_Image : constant String := C.unsigned_int'Image (Alpha);
-			begin
-				return Major_Image (Major_Image'First + 1 .. Major_Image'Last)
-					& '.' & Minor_Image (Minor_Image'First + 1 .. Minor_Image'Last)
-					& "alpha" & Alpha_Image (Alpha_Image'First + 1 .. Alpha_Image'Last);
-			end;
-		else
-			return Major_Image (Major_Image'First + 1 .. Major_Image'Last)
-				& '.' & Minor_Image (Minor_Image'First + 1 .. Minor_Image'Last);
-		end if;
+		pragma Assert (Micro_Image (Micro_Image'First) = ' ');
+		return Major_Image (Major_Image'First + 1 .. Major_Image'Last)
+			& '.' & Minor_Image (Minor_Image'First + 1 .. Minor_Image'Last)
+			& '.' & Micro_Image (Micro_Image'First + 1 .. Micro_Image'Last);
 	end Version;
 	
 	function Heap_Size return System.Storage_Elements.Storage_Count is
